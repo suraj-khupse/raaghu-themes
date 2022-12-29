e9f8716b929609494e34df21410bd89c69d0b282
Ketan-Khalate <99807624+Ketan-Khalate@users.noreply.github.com>
2022-12-29 17:10:42 +0530

    changes made in service file

--- projects/rds-themes/src/lib/rds-themes.service.ts
+++ projects/rds-themes/src/lib/rds-themes.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RdsThemesService {
+export class RdsThemesService implements OnInit {
 
-  constructor() { }
+  public static defaultThemeMode = 'light';
+  // public readonly style: HTMLLinkElement;
+
+  public get selectedThemeMode(): string {
+    return RdsThemesService.defaultThemeMode;
+  }
+
+  public set selectedThemeMode(value: string) {
+    RdsThemesService.defaultThemeMode = value;
+    this.style.href = `./styles/${value}.scss`;
+  }
+
+  constructor(
+    public style: HTMLLinkElement
+  ) { }
+
+  ngOnInit() {
+    this.style = document.createElement('link');
+    this.style.rel = 'stylesheet';
+    this.style.href = `./styles/${this.selectedThemeMode}.scss`;
+    document.head.appendChild(this.style);
+  }
+
+
+  setThemeMode(name: any) {
+    RdsThemesService.defaultThemeMode = name;
+    document.documentElement.setAttribute('theme', name);
+  }
 }
+
