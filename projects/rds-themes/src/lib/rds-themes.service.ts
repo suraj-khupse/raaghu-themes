b91780edd87cfbccd22b904967e27614ed0ed2ee
Alok Nakhate <alok.nakhate@waiin.com>
2022-12-30 12:13:54 +0530

    Update rds-themes.service.ts

--- projects/rds-themes/src/lib/rds-themes.service.ts
+++ projects/rds-themes/src/lib/rds-themes.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RdsThemesService  {
+export class RdsThemesService implements OnInit {
 
+  public static defaultTheme = 'default';
   public static defaultThemeMode = 'light';
+  private readonly style: HTMLLinkElement;
+
+  public get selectedTheme(): string {
+    return RdsThemesService.defaultTheme;
+  }
+
+  public set selectedTheme(value: string) {
+    RdsThemesService.defaultTheme = value;
+  }
 
   public get selectedThemeMode(): string {
     return RdsThemesService.defaultThemeMode;
@@ -13,25 +23,32 @@ export class RdsThemesService  {
 
   public set selectedThemeMode(value: string) {
     RdsThemesService.defaultThemeMode = value;
-    this.style.href = `./styles/${value}.scss`;
   }
 
-  private readonly style: HTMLLinkElement;
-
-  constructor(   
-  ) {
+  constructor() {
     this.style = document.createElement('link');
     this.style.rel = 'stylesheet';
-    // if(RdsThemesService.defaultThemeMode!==undefined){
-    //   this.style.href = `./styles/${this.selectedThemeMode}.scss`;
-    // } 
-   }
-
- 
-
-  set setThemeMode(name: any) {
-    // RdsThemesService.defaultThemeMode = name;
-    document.documentElement.setAttribute('theme', name);
+    document.head.appendChild(this.style);
   }
+
+  ngOnInit(): void {
+    if (this.selectedTheme !== undefined) {
+      this.style.href = `./styles/${this.selectedTheme}.scss`;
+    }
+    document.documentElement.setAttribute('theme', this.selectedThemeMode);
+  }
+
+  set setThemeMode(mode: any) {
+    this.selectedThemeMode = mode;
+    document.documentElement.setAttribute('theme', mode);
+  }
+
+  set setTheme(theme: string) {
+    this.selectedTheme = theme;
+    if (this.selectedTheme !== undefined) {
+      this.style.href = `./styles/${this.selectedTheme}.scss`;
+    }
+  }
+
 }
 
